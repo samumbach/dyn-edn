c62378a56cff0fa5474902088ebfc5ab424211c8
Sam Umbach <samumbach@gmail.com>
2025-06-09 11:05:13 -0400

    Rename test namespaces to comply with `some-ns-name-test` naming convention
    - Naming convention documented in the Clojure Style Guide: https://github.com/bbatsov/clojure-style-guide?tab=readme-ov-file#test-namespace-naming
    - This naming convention is assumed by common Clojure test runners:
        - https://github.com/cognitect-labs/test-runner/blob/v0.5.1/src/cognitect/test_runner.clj#L13
        - https://github.com/lambdaisland/kaocha/blob/v1.91.1392/resources/kaocha/default_config.edn#L9
        - https://github.com/clojure-emacs/cider/blob/v1.18.0/cider-test.el#L663-L670

--- test/com/walmartlabs/dyn_edn_test.clj
+++ test/com/walmartlabs/dyn_edn_test.clj
@@ -12,7 +12,7 @@
 ;;; See the License for the specific language governing permissions and
 ;;; limitations under the License."
 
-(ns com.walmartlabs.dyn-edn-tests
+(ns com.walmartlabs.dyn-edn-test
   (:require
     [clojure.test :refer [deftest is are]]
     [clojure.edn :as edn]
